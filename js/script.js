6cb6272934b0e40be2faccc98a95fed002a2e158
CarlosBF <cborrellf@outlook.es>
2025-03-11 12:57:53 +0100

    Ejercicio terminado

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
  // Aquí tu código
+const lista = document.getElementById('lista')
+const boton = document.getElementById('agregar')
+
+boton.addEventListener('click', function () {
+    let listItem = document.createElement('li')
+    listItem.innerText = prompt('Introduce un nuevo elemento a la lista:')
+    lista.appendChild(listItem)
+    lista.appendChild(document.createElement('hr'))
+})
